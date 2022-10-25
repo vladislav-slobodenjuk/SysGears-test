82a552c4fd44eba9fc475a30ffad4d605cc21924
vladislav-slobodenjuk <vladjun@gmail.com>
2022-10-26 01:20:43 +0300

    task-03 stage findPoint

--- js/tasks/task-03.js
+++ js/tasks/task-03.js
@@ -1,13 +1,12 @@
 console.log('This is task-03');
 
-const randomPoint = { x: 0, y: 0, z: 10 };
+// const randomPoint = { x: 0, y: 0, z: 10 };
 // const randomPoint = {
 //   x: Math.round(Math.random() * (10 - 1) + 1),
 //   y: Math.round(Math.random() * (10 - 1) + 1),
 //   z: Math.round(Math.random() * (10 - 1) + 1),
 // };
-console.log('randomPoint 0-10:', randomPoint);
-// console.log(Math.round(Math.random() * (10 - 1) + 1));
+// console.log('randomPoint 0-10:', randomPoint);
 
 function countDistance(obj, point) {
   return Math.sqrt(
@@ -15,4 +14,15 @@ function countDistance(obj, point) {
   );
 }
 
-console.log('distance:', countDistance({ x: 0, y: 0, z: 0 }, randomPoint));
+// console.log('distance:', countDistance({ x: 0, y: 0, z: 0 }, randomPoint));
+
+function findPoint() {
+  const randomPoint = { x: 0, y: 0, z: 10 };
+  let calls = 0;
+  let point = { x: 0, y: 0, z: 0 };
+  let distance = 0;
+  // console.log('distance :>> ', distance);
+  //
+}
+
+console.log(findPoint());
